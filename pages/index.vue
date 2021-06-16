580697d771b4a9dcf9c1fd651fce3418a0b0ae16
luzhenqian <15753140326@163.com>
2021-06-17 01:06:19 +0800

    feat: style and position

--- pages/index.vue
+++ pages/index.vue
@@ -7,7 +7,18 @@
       backgroundColor: pageData.backgroundColor,
     }"
   >
-    <component :is="button">hi</component>
+    <component
+      v-for="component in pageData.components"
+      :key="component.id"
+      :is="component.componentID"
+      :style="{
+        width: `${component.config.style.size.width}px`,
+        height: `${component.config.style.size.height}px`,
+        top: `${component.config.style.position.y}px`,
+        left: `${component.config.style.position.x}px`,
+      }"
+      >hi</component
+    >
   </div>
 </template>
 
@@ -20,16 +31,32 @@ Vue.use(AntD)
 export default Vue.extend({
   data() {
     return {
-      pageData: {},
+      pageData: {
+        name: '',
+        width: 1920,
+        height: 1080,
+        backgroundColor: 'red',
+        components: [
+          {
+            id: 121,
+            componentID: 'a-button',
+            config: {
+              style: {
+                size: { width: 100, heigth: 200 },
+                position: { x: 300, y: 400 },
+              },
+            },
+          },
+        ],
+      },
       AntD,
-      button: 'a-button',
     }
   },
   async fetch() {
     // password 111111
-    this.pageData = await this.$axios
-      .$get('http://www.mockhttp.cn/mock/page_data')
-      .then((res) => res)
+    // this.pageData = await this.$axios
+    //   .$get('http://www.mockhttp.cn/mock/page_data')
+    //   .then((res) => res)
   },
 })
 </script>
@@ -39,5 +66,9 @@ export default Vue.extend({
   padding: 0;
   margin: 0;
   position: relative;
+}
+
+.container > * {
+  position: absolute;
 }
 </style>
